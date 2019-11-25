80364cf3d461d9910c7be4786e690fa237c21a46
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-11-25 10:39:51 -0300

    estrutura projeto

--- app/controllers/Cartão.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db');
-
-// index, show, store, update, destroy
-module.exports = {
-  store(req, res) {
-    //Constroi a query e retorna o res 
-  },
-};
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+
+const Cartao = require("./controllers/Cartao");
+const Compra = require("./controllers/Compra");
+const Boleto = require("./controllers/Boleto");
+const Loja = require("./controllers/Loja");
+const Transacao = require("./controllers/Transacao");
+const Comprador = require("./controllers/Comprador");
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+routes.get("/", (req, res) => {
+    res.json({ message: "Hello World!" });
 });
+
+routes.get("/transacao", Transacao.index);
 
 module.exports = routes;
