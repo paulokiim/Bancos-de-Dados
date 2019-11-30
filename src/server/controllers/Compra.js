b11acb5d9ab69dc79f1df5654ea427c56b2c2ba4
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-11-30 17:31:11 -0300

    get compra

--- src/server/controllers/Compra.js
+++ src/server/controllers/Compra.js
@@ -1,9 +1,10 @@
-const db = require("../db");
+const db = require("../db")
 
 module.exports = {
-  show(req, res) {
-    const { id_comprador } = req.query;
-    const text = 'select c.id_compra, c.valor, c.data, cartao.digitos, cartao.portador FROM bd.compra c left join bd.comprador cp on (cp.id_comprador = $1) left join bd.cartao cartao on (cartao.fk_id_comprador = $2);'
+  index(req, res) {
+    const { id_comprador } = req.query
+    const text =
+      "select c.id_compra, c.valor, c.data, cartao.digitos, cartao.portador FROM bd.compra c left join bd.comprador cp on (cp.id_comprador = $1) left join bd.cartao cartao on (cartao.fk_id_comprador = $2);"
     const params = [id_comprador, id_comprador]
 
     db.query(text, params, (err, result) => {
@@ -11,59 +12,61 @@ module.exports = {
         console.log(err.stack)
       } else {
         console.log(result.rows[0])
-        res.send(result.rows.map((item) => {
-          return {
-            ...item,
-            valor: parseFloat(item.valor/100).toFixed(2),
-          }
-        }));
-      } 
-    });
+        res.send(
+          result.rows.map(item => {
+            return {
+              ...item,
+              valor: parseFloat(item.valor / 100).toFixed(2)
+            }
+          })
+        )
+      }
+    })
   },
 
   create(req, res) {
-    const { id_comprador, id_loja, valor, metodo } = req.body;
+    const { id_comprador, id_loja, valor, metodo } = req.body
 
-    const parsedValor = Math.floor(valor * 100);
-    const data = new Date().toISOString();
-    const data_futura = new Date(parseInt(data) + 1000000).toISOString();
+    const parsedValor = Math.floor(valor * 100)
+    const data = new Date().toISOString()
+    const data_futura = new Date(parseInt(data) + 1000000).toISOString()
 
     const compraText =
-      "INSERT INTO bd.compra (fk_id_comprador, fk_id_loja, data, valor) VALUES ($1, $2, $3, $4) RETURNING *;";
-    const compraValues = [id_comprador, id_loja, data, parsedValor];
+      "INSERT INTO bd.compra (fk_id_comprador, fk_id_loja, data, valor) VALUES ($1, $2, $3, $4) RETURNING *;"
+    const compraValues = [id_comprador, id_loja, data, parsedValor]
 
     db.query(compraText, compraValues, (err, result) => {
       if (err) {
-        console.log(err.stack);
+        console.log(err.stack)
       } else {
-        console.log(result.rows[0]);
-        const resultado = result.rows[0];
+        console.log(result.rows[0])
+        const resultado = result.rows[0]
 
         const transacaoText =
-          "INSERT INTO bd.transacao (fk_id_compra, data, valor, metodo, status) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
+          "INSERT INTO bd.transacao (fk_id_compra, data, valor, metodo, status) VALUES ($1, $2, $3, $4, $5) RETURNING *;"
         const transacaoValues = [
           resultado.id_compra,
           data,
           parsedValor,
           metodo,
           1
-        ];
+        ]
 
         db.query(transacaoText, transacaoValues, (err, result) => {
           if (err) {
-            console.log(err.stack);
+            console.log(err.stack)
           } else {
-            console.log(result.rows[0]);
-            const transacao = result.rows[0];
+            console.log(result.rows[0])
+            const transacao = result.rows[0]
 
             if (metodo === "boleto") {
-              const recebedor = "Lojinha BD";
-              const { id_transacao } = transacao;
-              const { id_compra, fk_id_comprador } = resultado;
+              const recebedor = "Lojinha BD"
+              const { id_transacao } = transacao
+              const { id_compra, fk_id_comprador } = resultado
               const codigo_barra =
-                "34191.79001 01043.510047 91020.150008 2 80870026000";
+                "34191.79001 01043.510047 91020.150008 2 80870026000"
               const boletoText =
-                "INSERT INTO bd.boleto (fk_id_transacao, fk_id_compra, codigo_barra, valor, vencimento, pagador, recebedor) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
+                "INSERT INTO bd.boleto (fk_id_transacao, fk_id_compra, codigo_barra, valor, vencimento, pagador, recebedor) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *"
               const boletoValues = [
                 id_transacao,
                 id_compra,
@@ -72,20 +75,20 @@ module.exports = {
                 data_futura,
                 fk_id_comprador,
                 recebedor
-              ];
+              ]
 
               db.query(boletoText, boletoValues, (err, result) => {
                 if (err) {
-                  console.log(err.stack);
+                  console.log(err.stack)
                 } else {
-                  console.log(result.rows[0]);
-                  res.send(201, result.rows[0]);
+                  console.log(result.rows[0])
+                  res.send(201, result.rows[0])
                 }
-              });
+              })
             }
           }
-        });
+        })
       }
-    });
+    })
   }
-};
+}
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,22 +1,22 @@
-const express = require('express')
+const express = require("express")
 
-const Cartao = require('./controllers/Cartao')
-const Compra = require('./controllers/Compra')
-const Comprador = require('./controllers/Comprador')
+const Cartao = require("./controllers/Cartao")
+const Compra = require("./controllers/Compra")
+const Comprador = require("./controllers/Comprador")
 
 const routes = express.Router()
 
 // Cartões
-routes.get('/cartoes', Cartao.show)
-routes.post('/cartoes', Cartao.create)
-routes.delete('/cartoes', Cartao.delete)
+routes.get("/cartoes", Cartao.show)
+routes.post("/cartoes", Cartao.create)
+routes.delete("/cartoes", Cartao.delete)
 
 // Compras
-routes.get('/compras', Compra.show)
-routes.post('/compras', Compra.create)
+routes.get("/compras", Compra.index)
+routes.post("/compras", Compra.create)
 
 // Compradores
-routes.get('/compradores', Comprador.show)
-routes.post('/compradores', Comprador.create)
+routes.get("/compradores", Comprador.show)
+routes.post("/compradores", Comprador.create)
 
 module.exports = routes
