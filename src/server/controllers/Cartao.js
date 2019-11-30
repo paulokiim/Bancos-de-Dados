815f45efbfa1dbef2ead11c6f3506836fef0f16e
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-11-30 00:02:43 -0300

    rotas dos cartoes ok

--- src/server/controllers/Cartao.js
+++ src/server/controllers/Cartao.js
@@ -5,29 +5,22 @@ module.exports = {
   show(req, res) {
     const { digitos } = req.query
 
-    let cartoes = [{}]
     const text = "SELECT * FROM bd.cartao WHERE digitos = $1"
     const values = [digitos]
 
-    let resposta = null
     db.query(text, values, (err, result) => {
       if (err) {
         console.log(err.stack)
+        res.status(400).send({ error: "Nao foi possivel realizar a consulta" })
       } else {
         // console.log("Show Cartoes", result.rows[0])
-        resposta = result.rows[0]
+        res.send(result.rows[0])
       }
     })
-    // console.log(digitos, cartoes)
-    res.send(resposta)
   },
 
   create(req, res) {
     const { id_comprador, bandeira, portador, digitos } = req.body
-
-    // console.log("tipo", typeof digitos)
-    // const digitosTratados = digitos.trim()
-    // console.log(digitosTratados)
 
     const text =
       "INSERT INTO bd.cartao (fk_id_comprador, bandeira, portador, digitos) VALUES ($1, $2, $3, $4) RETURNING *;"
@@ -45,7 +38,18 @@ module.exports = {
 
   delete(req, res) {
     const { digitos } = req.query
-    cartoes = cartoes.filter(cartao => cartao.digitos != digitos)
-    res.sendStatus(200)
+
+    const text = "DELETE FROM bd.cartao WHERE digitos = $1 RETURNING *;"
+    const values = [digitos]
+
+    db.query(text, values, (err, result) => {
+      if (err) {
+        console.log(err.stack)
+        res.status(204).send({ error: "Nao foi possivel deletar" })
+      } else {
+        console.log(result.rows[0])
+        res.send(200, result.rows[0])
+      }
+    })
   }
 }
